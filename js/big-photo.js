261ef35e222a72ffcdcc4e727148d15f04b6434e
Kotwade <kotwade@yandex.ru>
2022-05-26 14:41:26 +0500

    Проект кекстаграм

---
+++ js/big-photo.js
@@ -0,0 +1,52 @@
+const bigPicture = document.querySelector('.big-picture');
+const scrollOff = document.querySelector('body');
+const bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
+
+const commentsCount = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+commentsCount.classList.add('hidden');
+commentsLoader.classList.add('hidden');
+
+const onBigPictureCloseClick = () => {
+  bigPicture.classList.add('hidden');
+  scrollOff.classList.remove('modal-open');
+  bigPictureClose.removeEventListener('click', onBigPictureCloseClick);
+  commentList.innerHTML = '';
+};
+
+const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+const commentList = document.querySelector('.social__comments');
+
+const renderComment = (comment) => {
+  const commentSimilar = commentTemplate.cloneNode(true);
+
+  commentSimilar.querySelector('.social__picture').src = comment.avatar;
+  commentSimilar.querySelector('.social__picture').alt = comment.name;
+  commentSimilar.querySelector('.social__text').textContent = comment.message;
+
+  return commentSimilar;
+};
+
+const renderComments = (comments) => {
+  let commentsListFragment = document.createDocumentFragment();
+
+  comments.forEach(comment => {
+    commentsListFragment.appendChild(renderComment(comment));
+  });
+
+  commentList.appendChild(commentsListFragment);
+};
+
+const show = (picture) => {
+  scrollOff.classList.add('modal-open');
+  bigPicture.querySelector('.big-picture__img > img').src = picture.url;
+  bigPicture.querySelector('.likes-count').textContent = picture.likes;
+  bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
+  bigPicture.querySelector('.social__caption').textContent = picture.description;
+
+  bigPictureClose.addEventListener('click', onBigPictureCloseClick);
+  renderComments(picture.comments);
+  bigPicture.classList.remove('hidden');
+};
+
+export { show };
---
+++ js/main.js
@@ -0,0 +1,5 @@
+import './editor.js';
+import './effects.js';
+import { renderPhotos } from './preview.js';
+
+renderPhotos();
